ebdf084976d9d76b335d2a0843893f2eb1815f76
Harshsharma3737 <harsharma3737@gmail.com>
2026-09-06 17:52:10 +0530

    Allow mobile local IP in allowedDevOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
     unoptimized: true,
   },
   devIndicators: false,
+  allowedDevOrigins: [
+    "192.168.29.175",
+    "192.168.29.175:8080",
+    "localhost",
+    "localhost:8080",
+    "127.0.0.1",
+    "127.0.0.1:8080",
+  ],
 };
 
 export default nextConfig;
